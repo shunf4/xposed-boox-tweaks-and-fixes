79f31fd0f1a8ea3653fb695118d98e27e228095d
shunf4 <shun1048576@gmail.com>
2021-02-03 23:10:14 +0800

    feat: more NavBar tunes & make configurable

--- README.md
+++ README.md
@@ -1,7 +1,40 @@
 ## XBTNF
 
-Various Fixs and Tweaks to BOOX as a Xposed module
+Various Fixs and Tweaks to BOOX as a Xposed module.
 
-- Fixes crash when tapping navigation bar button after enabling navigation bar
-- Make `Alt+Tab` able to bring up Overview (Recent Task Switcher)
-- Allow changing the E-Ink optimization configuration for system(whitelisted) apps (Be careful!)
+Only tested on Max3 with ROM 2020-11-04_07_07_3.0_9aed398. Using this module could cause harm to your device. No warranty is assured!
+
+### Usage
+
+0. Backup all data on your device
+1. Disable App List mode in EdXposed Manager
+2. Tick this module in EdXposed Manager
+3. Create files in `/data/local` with root permission if you want to switch on/off some tweaks;
+    See below
+4. Reboot
+5. Enjoy
+
+### Features
+
+**You need to install and enable the [Show Navigation Bar](https://github.com/shunf4/xposed-boox-tweaks-and-fixes/files/5919306/Show_navigation_bar-v06.zip) Magisk module to enjoy (the navigation bar and) the tweaks in this Xposed module.**
+
+Also, you are advised to install [SystemUI Tuner Redesigned](https://play.google.com/store/apps/details?id=com.zacharee1.systemuituner&hl=en_US) to tune more things (per-app immersive mode etc.)
+
+- Fix crash when tapping navigation bar buttons after enabling navigation bar  
+    (Default is enabled; to disable, create the file `/data/local/xbtnf_disable_navbar_button_fix`)
+
+- Force NavBar to always be the same color (light/white)
+    (Default is enabled (light color); to disable, create the file `/data/local/xbtnf_disable_navbar_color_tweak`.
+    If you want NavBar to always be dark, create the file `/data/local/xbtnf_navbar_dark`.)
+
+- Fix the form of NavBar, switching the original ["SwipeUpUI" NavBar](https://www.androidpolice.com/2018/05/14/android-ps-new-overview-app-switcher-part-launcher/) to a normal form with back/home/recent buttons  
+    (Default is enabled; to disable, create the file `/data/local/xbtnf_disable_navbar_form_fix`)
+
+- Change the layout of NavBar buttons with a layout string (Please decompile `SystemUI.apk` from the ROM and see in `NavigationBarInflaterView` class how your BOOX handles the string and generate the layout. Here's the [source code](https://github.com/aosp-mirror/platform_frameworks_base/blob/pie-release/packages/SystemUI/src/com/android/systemui/statusbar/phone/NavigationBarInflaterView.java) just for reference)  
+    (Default is enabled, changing the layout to `left[.5W],back[1WC];space[1W],home[1WC],space[1W];recent[1WC],right[.5W]`; to change the layout string, create the file `/data/local/xbtnf_navbar_layout` and write the string into it. If you write nothing more than whitespaces, the fix is disabled, falling back to the default string `back,home,left;space;right,recent` which only shows a single "back" button for unknown reason)
+
+- Make `Alt+Tab` able to bring up Overview (Recent Task Switcher)  
+    (Default is enabled; to disable, create the file `/data/local/xbtnf_disable_alt_tab_overview_tweak`)
+
+- Allow changing the E-Ink optimization configuration for system (whitelisted) apps (Be careful!)
+    (Default is enabled; to disable, create the file `/data/local/xbtnf_disable_all_apps_eink_optimizable_tweak`)
--- app/src/main/java/com/shunf4/xposedbooxtweaknfix/ModMain.java
+++ app/src/main/java/com/shunf4/xposedbooxtweaknfix/ModMain.java
@@ -5,75 +5,139 @@ import android.app.AndroidAppHelper;
 import android.content.ComponentName;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.Rect;
+import android.os.Build;
+import android.util.Log;
+import android.view.View;
 import android.widget.Toast;
 
+import androidx.annotation.RequiresApi;
+
+import java.io.File;
+import java.io.FileReader;
 import java.io.PrintWriter;
 import java.io.StringWriter;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
 import java.util.List;
 
+import de.robv.android.xposed.IXposedHookInitPackageResources;
 import de.robv.android.xposed.IXposedHookLoadPackage;
+import de.robv.android.xposed.IXposedHookZygoteInit;
 import de.robv.android.xposed.XC_MethodHook;
 import de.robv.android.xposed.XposedBridge;
 import de.robv.android.xposed.XposedHelpers;
+import de.robv.android.xposed.callbacks.XC_InitPackageResources;
 import de.robv.android.xposed.callbacks.XC_LoadPackage.LoadPackageParam;
 
-public class ModMain implements IXposedHookLoadPackage {
+public class ModMain implements IXposedHookLoadPackage, IXposedHookZygoteInit, IXposedHookInitPackageResources {
+    private static final String FLAGS_DIRECTORY_PREFIX = "/data/local/xbtnf_";
+
+    private static final boolean shouldDisableNavBarButtonFix =
+            new File(FLAGS_DIRECTORY_PREFIX + "disable_navbar_button_fix").exists();
+    private static final boolean shouldDisableAltTabOverviewTweak =
+            new File(FLAGS_DIRECTORY_PREFIX + "disable_alt_tab_overview_tweak").exists();
+    private static final boolean shouldDisableNavBarFormFix =
+            new File(FLAGS_DIRECTORY_PREFIX + "disable_navbar_form_fix").exists();
+    private static final boolean shouldNavBarBeDark =
+            new File(FLAGS_DIRECTORY_PREFIX + "navbar_dark").exists();
+    private static final boolean shouldDisableNavBarColorTweak =
+            new File(FLAGS_DIRECTORY_PREFIX + "disable_navbar_color_tweak").exists();
+    private static final boolean shouldDisableAllAppsEInkOptimizableTweak =
+            new File(FLAGS_DIRECTORY_PREFIX + "disable_all_apps_eink_optimizable_tweak").exists();
+
+
     public void handleLoadPackage(final LoadPackageParam lpparam) throws Throwable {
         if (lpparam.packageName.equals("com.android.systemui")) {
             XposedBridge.log("xbtnf: loaded app: " + lpparam.packageName);
 
-            // Fix crash on touching NavBar buttons
-            XposedHelpers.findAndHookMethod("android.view.MotionEvent", lpparam.classLoader, "recycle", new XC_MethodHook() {
-                @Override
-                protected void beforeHookedMethod(MethodHookParam mhparam) throws Throwable {
-                    StringWriter sw = new StringWriter();
-                    PrintWriter pw = new PrintWriter(sw);
-                    new Exception().printStackTrace(pw);
-                    String stackTraceStr = sw.toString();
+            if (!shouldDisableNavBarButtonFix) {
+                // Fix crash on touching NavBar buttons
+                XposedHelpers.findAndHookMethod("android.view.MotionEvent", lpparam.classLoader, "recycle", new XC_MethodHook() {
+                    @Override
+                    protected void beforeHookedMethod(MethodHookParam mhparam) throws Throwable {
+                        StringWriter sw = new StringWriter();
+                        PrintWriter pw = new PrintWriter(sw);
+                        new Exception().printStackTrace(pw);
+                        String stackTraceStr = sw.toString();
 
-                    // XposedBridge.log("xbtnf: before MotionEvent.recycle: " + stackTraceStr);
+                        // XposedBridge.log("xbtnf: before MotionEvent.recycle: " + stackTraceStr);
 
-                    StackTraceElement[] stes = Thread.currentThread().getStackTrace();
-                    StackTraceElement targetSte = null;
-                    for (StackTraceElement ste : stes) {
-                        if (ste.getClassName().contains("com.android.quickstep.TouchInteractionService")
-                                && ste.getMethodName().equals("onMotionEvent")) {
-                            targetSte = ste;
-                            break;
+                        StackTraceElement[] stes = Thread.currentThread().getStackTrace();
+                        StackTraceElement targetSte = null;
+                        for (StackTraceElement ste : stes) {
+                            if (ste.getClassName().contains("com.android.quickstep.TouchInteractionService")
+                                    && ste.getMethodName().equals("onMotionEvent")) {
+                                targetSte = ste;
+                                break;
+                            }
                         }
-                    }
 
-                    if (targetSte == null) {
+                        if (targetSte == null) {
                         /* XposedBridge.log("xbtnf: before MotionEvent.recycle: " +
                                 "com.android.quickstep.TouchInteractionService$3.onMotionEvent " +
                                 "not found in stack trace"); */
-                    } else {
-                        // XposedBridge.log("xbtnf: before MotionEvent.recycle: " + stackTraceStr);
-                        XposedBridge.log("xbtnf: before MotionEvent.recycle: " +
-                                "target found. fix!");
-                        mhparam.setResult(null);
+                        } else {
+                            // XposedBridge.log("xbtnf: before MotionEvent.recycle: " + stackTraceStr);
+                            XposedBridge.log("xbtnf: before MotionEvent.recycle: " +
+                                    "target found. fix!");
+                            mhparam.setResult(null);
+                        }
                     }
-                }
-            });
+                });
+            }
 
-            // Make Alt-Tab call Overview (Recents)
-            XposedHelpers.findAndHookMethod("com.android.quickstep.TouchInteractionService$3", lpparam.classLoader, "onOverviewShown", boolean.class, new XC_MethodHook() {
-                @Override
-                protected void afterHookedMethod(MethodHookParam mhparam) throws Throwable {
-                    boolean triggeredFromAltTab = (Boolean) mhparam.args[0];
-                    if (triggeredFromAltTab) {
-                        XposedBridge.log("xbtnf: triggered Overview from AltTab. starting Overview");
-                        Intent intent = new Intent();
-                        intent.setComponent(new ComponentName("com.android.systemui", "com.android.systemui.recents.OnyxRecentsActivity"));
-                        intent.setFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION | Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_RESET_TASK_IF_NEEDED);
-                        AndroidAppHelper.currentApplication().startActivity(intent);
+            if (!shouldDisableAltTabOverviewTweak) {
+                // Make Alt-Tab call Overview (Recents)
+                XposedHelpers.findAndHookMethod("com.android.quickstep.TouchInteractionService$3", lpparam.classLoader, "onOverviewShown", boolean.class, new XC_MethodHook() {
+                    @Override
+                    protected void afterHookedMethod(MethodHookParam mhparam) throws Throwable {
+                        boolean triggeredFromAltTab = (Boolean) mhparam.args[0];
+                        if (triggeredFromAltTab) {
+                            XposedBridge.log("xbtnf: triggered Overview from AltTab. starting Overview");
+                            Intent intent = new Intent();
+                            intent.setComponent(new ComponentName("com.android.systemui", "com.android.systemui.recents.OnyxRecentsActivity"));
+                            intent.setFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION | Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_RESET_TASK_IF_NEEDED);
+                            AndroidAppHelper.currentApplication().startActivity(intent);
+                        }
                     }
-                }
-            });
+                });
+            }
+
+            if (!shouldDisableNavBarFormFix) {
+                // Force NavBar to use the normal form (without SwipeUpUI)
+                XposedHelpers.findAndHookMethod("com.android.systemui.OverviewProxyService",
+                        lpparam.classLoader, "isEnabled", new XC_MethodHook() {
+                            @Override
+                            protected void beforeHookedMethod(MethodHookParam mhparam) throws Throwable {
+                                mhparam.setResult(Boolean.FALSE);
+                            }
+                        });
+            }
+
+            if (!shouldDisableNavBarColorTweak) {
+                // Force NavBar to use dark/light icons
+                XposedHelpers.findAndHookMethod("com.android.systemui.statusbar.phone.LightBarController",
+                        lpparam.classLoader, "isLightNavigationBarMode", int.class, new XC_MethodHook() {
+                            @Override
+                            protected void beforeHookedMethod(MethodHookParam mhparam) throws Throwable {
+                                mhparam.setResult(!shouldNavBarBeDark);
+                            }
+                        });
+
+                XposedHelpers.findAndHookMethod("com.android.systemui.statusbar.phone.LightBarController",
+                        lpparam.classLoader, "isNavigationLight", new XC_MethodHook() {
+                            @Override
+                            protected void beforeHookedMethod(MethodHookParam mhparam) throws Throwable {
+                                mhparam.setResult(!shouldNavBarBeDark);
+                            }
+                        });
+            }
         }
 
-        // Make system apps optimizable
-        {
+        if (!shouldDisableAllAppsEInkOptimizableTweak) {
+            // Make system apps E-Ink screen optimizable
             if (lpparam.packageName.equals("com.onyx.floatingbutton")) {
                 XposedBridge.log("xbtnf: loaded app: " + lpparam.packageName);
 
@@ -81,10 +145,14 @@ public class ModMain implements IXposedHookLoadPackage {
                 XposedHelpers.findAndHookMethod("com.onyx.android.sdk.eac.v2.EACManager", lpparam.classLoader, "inEACWhiteList", String.class, new XC_MethodHook() {
                     @Override
                     protected void afterHookedMethod(MethodHookParam mhparam) throws Throwable {
-                        XposedBridge.log("xbtnf: inEACWhiteList(): fix!");
+                        // XposedBridge.log("xbtnf: inEACWhiteList(): fix!");
                         if ((Boolean) mhparam.getResult()) {
-                            Toast.makeText(AndroidAppHelper.currentApplication(), "Target is an app that should be exempt from optimization. " +
-                                    "Any modification to this app's optimization config may cause irreversible error.", Toast.LENGTH_LONG).show();
+                            try {
+                                Toast.makeText(AndroidAppHelper.currentApplication(), "Target is an app that should be exempt from optimization. " +
+                                        "Any modification to this app's optimization config may cause irreversible error.", Toast.LENGTH_LONG).show();
+                            } catch (Exception e) {
+                                ;
+                            }
                         }
                         mhparam.setResult(Boolean.FALSE);
                     }
@@ -93,7 +161,7 @@ public class ModMain implements IXposedHookLoadPackage {
                 XposedHelpers.findAndHookMethod("com.onyx.android.sdk.eac.v2.EACManager", lpparam.classLoader, "isEACEnabledApp", String.class, new XC_MethodHook() {
                     @Override
                     protected void beforeHookedMethod(MethodHookParam mhparam) throws Throwable {
-                        XposedBridge.log("xbtnf: isEACEnabledApp(): fix!");
+                        // XposedBridge.log("xbtnf: isEACEnabledApp(): fix!");
                         mhparam.setResult(Boolean.FALSE);
                     }
                 });
@@ -103,10 +171,64 @@ public class ModMain implements IXposedHookLoadPackage {
                 XposedHelpers.findAndHookMethod("android.onyx.optimization.OECService", lpparam.classLoader, "isPackageInEACWhiteSet", String.class, new XC_MethodHook() {
                     @Override
                     protected void beforeHookedMethod(MethodHookParam mhparam) throws Throwable {
-                        XposedBridge.log("xbtnf: OECService.isPackageInEACWhiteSet(): fix!");
+                        // XposedBridge.log("xbtnf: OECService.isPackageInEACWhiteSet(): fix!");
                         mhparam.setResult(Boolean.FALSE);
                     }
                 });
+            }
+        }
+    }
+
+    @Override
+    public void initZygote(StartupParam startupParam) throws Throwable {
+        if (!shouldDisableNavBarColorTweak) {
+            // Force NavBar to be light/dark
+            XposedHelpers.findAndHookMethod("com.android.internal.policy.DecorView", null, "calculateNavigationBarColor",
+                    new XC_MethodHook() {
+                        @Override
+                        protected void afterHookedMethod(MethodHookParam mhparam) throws Throwable {
+//                XposedBridge.log("xbtnf: after DecorView.calculateNavigationBarColor: changing return value " +
+//                        mhparam.getResult().toString()
+//                        + " to ..."
+//                );
+                            mhparam.setResult(
+                                    shouldNavBarBeDark ? Color.rgb(0, 0, 0) : Color.rgb(255, 255, 255)
+                            );
+                        }
+                    });
+        }
+    }
+
+    static final File navBarLayoutConfigFile = new File(FLAGS_DIRECTORY_PREFIX + "navbar_layout");
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    @Override
+    public void handleInitPackageResources(XC_InitPackageResources.InitPackageResourcesParam resparam) throws Throwable {
+        // Replace navBar layout
+        if (resparam.packageName.equals("com.android.systemui")) {
+            XposedBridge.log("xbtnf: loaded app resource: " + resparam.packageName);
+
+            if (navBarLayoutConfigFile.exists()) {
+                try {
+                    String navBarLayout = new String(Files.readAllBytes(navBarLayoutConfigFile.toPath()), StandardCharsets.US_ASCII);
+                    navBarLayout = navBarLayout.trim();
+
+                    if (!navBarLayout.equals("")) {
+                        resparam.res.setReplacement(
+                                "com.android.systemui",
+                                "string",
+                                "config_navBarLayout",
+                                navBarLayout);
+                    }
+                } catch (Exception e) {
+                    XposedBridge.log("xbtnf: on tweaking NavBar layout: " + e.toString());
+                }
+            } else {
+                resparam.res.setReplacement(
+                        "com.android.systemui",
+                        "string",
+                        "config_navBarLayout",
+                        "left[.5W],back[1WC];space[1W],home[1WC],space[1W];recent[1WC],right[.5W]");
             }
         }
     }
